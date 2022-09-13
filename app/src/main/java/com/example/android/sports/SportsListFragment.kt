7646ee2d9ffacce53745403a1bbda91638144b38
nijuyonkadesu <24ksriramv@gmail.com>
2022-09-13 07:49:39 +0530

    custom back press listener to make slidingPane close instead of closing app

--- app/src/main/java/com/example/android/sports/SportsListFragment.kt
+++ app/src/main/java/com/example/android/sports/SportsListFragment.kt
@@ -20,9 +20,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import androidx.slidingpanelayout.widget.SlidingPaneLayout
 import com.example.android.sports.databinding.FragmentSportsListBinding
 
 /**
@@ -36,24 +38,56 @@ class SportsListFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         return FragmentSportsListBinding.inflate(inflater, container, false).root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val binding = FragmentSportsListBinding.bind(view)
+        val slidingPaneLayout = binding.slidingPaneLayout
+        slidingPaneLayout.lockMode = SlidingPaneLayout.LOCK_MODE_LOCKED // disable swiping
+
+        // Connect the SlidingPaneLayout to the system back button. (ie, Register the callback)
+        requireActivity().onBackPressedDispatcher.addCallback(
+            viewLifecycleOwner, // onBackPressedDispatcher not active when fragment is not alive
+            SportsListOnBackPressedCallback(slidingPaneLayout)
+        )
 
         // Initialize the adapter and set it to the RecyclerView.
+        // TODO "figure out how does it get Sport as a parameter lolllllll 😭"
         val adapter = SportsAdapter {
             // Update the user selected sport as the current sport in the shared viewmodel
             // This will automatically update the dual pane content
             sportsViewModel.updateCurrentSport(it)
-            // Navigate to the details screen
-            val action = SportsListFragmentDirections.actionSportsListFragmentToNewsFragment()
-            this.findNavController().navigate(action)
+            // Overlaps SportsList with NewsDetails Fragment when width < 550dp or open sideways if > 550dp
+            binding.slidingPaneLayout.openPane()
         }
         binding.recyclerView.adapter = adapter
         adapter.submitList(sportsViewModel.sportsData)
+    }
+}
+class SportsListOnBackPressedCallback(
+    private val slidingPaneLayout: SlidingPaneLayout
+): OnBackPressedCallback(slidingPaneLayout.isSlideable && slidingPaneLayout.isOpen),
+    SlidingPaneLayout.PanelSlideListener{
+    init {
+        slidingPaneLayout.addPanelSlideListener(this) // without this, all of the below overridden methods are completely detached from back pressed callback
+    }
+    // pressing back in tablet is unnecessary.. coz page opens side by side. but on mobile,
+    // it overlaps SportsList hence need custom back key operation
+    override fun handleOnBackPressed() {
+        slidingPaneLayout.closePane()
+    }
+
+    override fun onPanelSlide(panel: View, slideOffset: Float) {
+    }
+
+    override fun onPanelOpened(panel: View) {
+        isEnabled = true // to enable invoke of handleOnBackPressed()
+    }
+
+    override fun onPanelClosed(panel: View) {
+        isEnabled = false // to disable invoke of handleOnBackPressed()
     }
 }
